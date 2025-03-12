24a111d2516f38df339cdf95130b9627a3714695
jang-199 <jw1010110@gmail.com>
2025-03-12 17:18:29 +0900

    Query test 작성

--- onething/boot/api/src/test/java/data/coredata/com/clip/user/entity/repository/UserRepositoryTest.java
+++ onething/boot/api/src/test/java/data/coredata/com/clip/user/entity/repository/UserRepositoryTest.java
@@ -1,15 +1,17 @@
 package data.coredata.com.clip.user.entity.repository;
 
 import com.clip.ApiApplication;
-import com.clip.user.entity.DeviceType;
-import com.clip.user.entity.User;
+import com.clip.user.entity.*;
 import com.clip.user.repository.UserRepository;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ContextConfiguration;
+
+import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -19,6 +21,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    EntityManager entityManager;
 
     @DisplayName("userId로 phoneNumber를 업데이트 한다.")
     @Test
@@ -117,6 +121,33 @@ public class UserRepositoryTest {
                         DeviceType.IOS,
                         osVersion,
                         firebaseToken
+                );
+    }
+
+    @DisplayName("userId로 gender, birth, city, county를 업데이트 한다.")
+    @Test
+    void updateUserDetailInfo() {
+        //given
+        LocalDate currentDate = LocalDate.now();
+        User user = userRepository.save(User.builder().build());
+
+        //when
+        userRepository.updateUserDetailInfo(user.getId(), Gender.FEMALE, currentDate, City.GYEONGGI, County.YONGIN_SI);
+        user = userRepository.findById(user.getId()).get();
+
+        //then
+        assertThat(user)
+                .extracting(
+                        User::getGender,
+                        User::getBirth,
+                        User::getCity,
+                        User::getCounty
+                )
+                .containsExactly(
+                        Gender.FEMALE,
+                        currentDate,
+                        City.GYEONGGI,
+                        County.YONGIN_SI
                 );
     }
 }
