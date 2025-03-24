a84a1df95bfd3d1216a630ee38198552f222b231
jang-199 <jw1010110@gmail.com>
2025-03-24 18:28:40 +0900

    DTO로 변경하는 Mapper method 추가

--- boot/api/src/main/java/com/clip/api/user/mapper/JobMapper.java
+++ boot/api/src/main/java/com/clip/api/user/mapper/JobMapper.java
@@ -1,6 +1,8 @@
 package com.clip.api.user.mapper;
 
 import com.clip.api.user.controller.dto.JobCategory;
+import com.clip.api.user.controller.dto.JobDto;
+import com.clip.user.entity.Job;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
@@ -13,5 +15,22 @@ public interface JobMapper {
         return jobCategoryList.stream()
                 .map(JobCategory::getJobCategoryName)
                 .toList();
+    }
+
+    default JobDto toJobDto(List<Job> jobList) {
+        return JobDto.builder()
+                .jobList(
+                        jobList.stream()
+                                .map(job -> {
+                                    for (JobCategory category : JobCategory.values()) {
+                                        if (category.getJobCategoryName().equals(job.getJobName())) {
+                                            return category;
+                                        }
+                                    }
+                                    throw new IllegalArgumentException("다음 이름은 DTO로 변환할 수 없는 이름입니다: " + job.getJobName());
+                                })
+                                .toList()
+                )
+                .build();
     }
 }
--- boot/api/src/main/java/com/clip/api/user/mapper/LanguageMapper.java
+++ boot/api/src/main/java/com/clip/api/user/mapper/LanguageMapper.java
@@ -1,13 +1,32 @@
 package com.clip.api.user.mapper;
 
 import com.clip.api.user.controller.dto.Language;
+import com.clip.api.user.controller.dto.LanguageDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
+
+import java.util.Objects;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface LanguageMapper {
     default String toStringLanguage(Language language) {
         return language.getValue();
+    }
+
+    default LanguageDto toLanguageDto(String language) {
+        if (Objects.isNull(language)) {
+            return LanguageDto.builder()
+                    .language(null)
+                    .build();
+        }
+        for (Language lang : Language.values()) {
+            if (lang.getValue().equals(language)) {
+                return LanguageDto.builder()
+                        .language(lang)
+                        .build();
+            }
+        }
+        throw new IllegalArgumentException("다음 언어는 DTO로 변환할 수 없는 언어입니다: " + language);
     }
 }
