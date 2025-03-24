c451d6312d6b25aac7bfdad1ff1d312d92e15dbb
jang-199 <jw1010110@gmail.com>
2025-03-24 18:27:44 +0900

    Dto 공통 사용을 위해 이름 변경 및 빌더 추가

--- boot/api/src/main/java/com/clip/api/user/controller/dto/LanguageDto.java
+++ boot/api/src/main/java/com/clip/api/user/controller/dto/LanguageDto.java
@@ -1,10 +1,16 @@
 package com.clip.api.user.controller.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class UpdateLanguageDto {
+public class LanguageDto {
     private Language language;
+
+    @Builder
+    public LanguageDto(Language language) {
+        this.language = language;
+    }
 }
--- boot/api/src/main/java/com/clip/api/user/controller/dto/RelationshipDto.java
+++ boot/api/src/main/java/com/clip/api/user/controller/dto/RelationshipDto.java
@@ -2,6 +2,7 @@ package com.clip.api.user.controller.dto;
 
 import com.clip.user.entity.RelationshipStatus;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,8 +10,14 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
-public class UpdateRelationshipDto {
+public class RelationshipDto {
     private RelationshipStatus relationshipStatus;
     @JsonProperty(value = "isSameRelationshipConsidered")
-    private boolean isSameRelationshipConsidered;
+    private Boolean isSameRelationshipConsidered;
+
+    @Builder
+    public RelationshipDto(RelationshipStatus relationshipStatus, Boolean isSameRelationshipConsidered) {
+        this.relationshipStatus = relationshipStatus;
+        this.isSameRelationshipConsidered = isSameRelationshipConsidered;
+    }
 }
