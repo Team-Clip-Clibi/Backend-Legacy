144323b6a81eccc0b7a75253f597189cf836f0c9
jang-199 <jw1010110@gmail.com>
2025-03-29 06:53:27 +0900

    repository 구현

---
+++ data/core-data/src/main/java/com/clip/matching/repository/OneThingMatchingRepository.java
@@ -0,0 +1,7 @@
+package com.clip.matching.repository;
+
+import com.clip.matching.entity.OneThingMatching;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OneThingMatchingRepository extends JpaRepository<OneThingMatching, Long> {
+}
