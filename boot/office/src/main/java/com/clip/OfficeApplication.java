70b19de975d97433e80394c395f6818fc6645ee8
isprogrammingfun <wisjang1@naver.com>
2025-03-29 00:03:24 +0900

    어드민 화면 기본 layout 설정

--- boot/office/src/main/java/com/clip/OfficeApplication.java
+++ boot/office/src/main/java/com/clip/OfficeApplication.java
@@ -1,8 +1,12 @@
-package com.clip.office;
+package com.clip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class OfficeApplication {
 
