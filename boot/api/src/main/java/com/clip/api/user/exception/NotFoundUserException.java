b9df750d4bb14668f4cd8ed618c2e561f09ef61f
jang-199 <jw1010110@gmail.com>
2025-03-30 11:17:50 +0900

    NotFoundUserException.java delete

--- boot/api/src/main/java/com/clip/api/user/exception/NotFoundUserException.java
+++
@@ -1,7 +0,0 @@
-package com.clip.api.user.exception;
-
-public class NotFoundUserException extends RuntimeException {
-    public NotFoundUserException() {
-        super("가입되지 않은 유저입니다.");
-    }
-}
