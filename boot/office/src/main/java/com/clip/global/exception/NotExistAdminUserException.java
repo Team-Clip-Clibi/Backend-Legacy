28c1356630959c200b725df51c65a4f48dc3e05a
W.llama <tmdqja96@gmail.com>
2025-03-31 21:26:29 +0900

    refactor 명확한 위치로 변경 및 상속 명시적으로 변경

--- boot/office/src/main/java/com/clip/global/exception/NotExistAdminUserException.java
+++
@@ -1,7 +0,0 @@
-package com.clip.global.exception;
-
-public class NotExistAdminUserException extends RuntimeException {
-    public NotExistAdminUserException() {
-        super("username 파라미터가 비어있습니다.");
-    }
-}
