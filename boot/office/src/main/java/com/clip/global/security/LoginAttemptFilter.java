4a1bb9168f56f0cc59a9694af940840e833c33fa
W.llama <tmdqja96@gmail.com>
2025-03-31 23:46:50 +0900

    사용하지 않은 예외 제거

--- boot/office/src/main/java/com/clip/global/security/LoginAttemptFilter.java
+++ boot/office/src/main/java/com/clip/global/security/LoginAttemptFilter.java
@@ -1,7 +1,6 @@
 package com.clip.global.security;
 
 import com.clip.global.security.util.LoginAttemptManager;
-import com.clip.office.admin.exception.NotExistAdminUserException;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -34,13 +33,8 @@ public class LoginAttemptFilter implements Filter {
                     response.sendRedirect("/office/admin/login?locked=true");
                     return;
                 }
-            } else {
-                log.atError()
-                        .setCause(new NotExistAdminUserException())
-                        .log();
             }
         }
-
         chain.doFilter(req, res);
     }
 }
--- boot/office/src/main/java/com/clip/office/admin/exception/NotExistAdminUserException.java
+++
@@ -1,9 +0,0 @@
-package com.clip.office.admin.exception;
-
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public class NotExistAdminUserException extends UsernameNotFoundException {
-    public NotExistAdminUserException() {
-        super("username 파라미터가 비어있습니다.");
-    }
-}
