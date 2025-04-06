12fef2243d6c1f773d242d7c017557de4b137f70
W.llama <tmdqja96@gmail.com>
2025-04-07 00:53:46 +0900

    feat banner 엔티티

---
+++ data/office-data/src/main/java/com/clip/notification/entity/Banner.java
@@ -0,0 +1,34 @@
+package com.clip.notification.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Banner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String exposureLocation;
+    private String head;
+    private String sub;
+    private String imageUrl;
+    private LocalDate exposureDate;
+    private boolean isExposure;
+
+    @Builder
+    public Banner(String exposureLocation, String head, String sub, String imageUrl, LocalDate exposureDate, boolean isExposure) {
+        this.exposureLocation = exposureLocation;
+        this.head = head;
+        this.sub = sub;
+        this.imageUrl = imageUrl;
+        this.exposureDate = exposureDate;
+        this.isExposure = isExposure;
+    }
+}
