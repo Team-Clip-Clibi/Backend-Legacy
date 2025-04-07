879412ede7c722760351e7f5e2b728c9b3d5b203
W.llama <tmdqja96@gmail.com>
2025-04-07 21:46:37 +0900

    사용하지 않은 메서드 제거(findAll)

--- data/core-data/src/main/java/com/clip/matching/service/RandomMatchingDataService.java
+++ data/core-data/src/main/java/com/clip/matching/service/RandomMatchingDataService.java
@@ -3,8 +3,6 @@ package com.clip.matching.service;
 import com.clip.matching.entity.RandomMatching;
 import com.clip.matching.repository.RandomMatchingRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,7 +15,4 @@ public class RandomMatchingDataService {
         return randomMatchingRepository.save(randomMatching);
     }
 
-    public Page<RandomMatching> findAll(Pageable pageable) {
-        return randomMatchingRepository.findAll(pageable);
-    }
 }
