703a51574803c214a4db85508954937c6c08e1e9
W.llama <tmdqja96@gmail.com>
2025-04-11 22:40:05 +0900

    randomMatching 레포지토리 delete 메소드 작성

--- data/core-data/src/main/java/com/clip/matching/repository/RandomMatchingRepository.java
+++ data/core-data/src/main/java/com/clip/matching/repository/RandomMatchingRepository.java
@@ -2,6 +2,14 @@ package com.clip.matching.repository;
 
 import com.clip.matching.entity.RandomMatching;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface RandomMatchingRepository extends JpaRepository<RandomMatching, Long> {
+    @Transactional
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("DELETE FROM RandomMatching rm WHERE rm.id = :randomMatchingId")
+    void deleteRandomMatching(@Param("randomMatchingId")Long randomMatchingId);
 }
