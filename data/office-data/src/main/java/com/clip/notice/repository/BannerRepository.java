eb64e686cbe39400f2f9b2901bea229a6aef1bf2
W.llama <tmdqja96@gmail.com>
2025-04-11 20:56:23 +0900

    사용안하는 update 쿼리 삭제 (더티체킹방향으로 처리해서 사용안함)

--- data/office-data/src/main/java/com/clip/notice/repository/BannerRepository.java
+++ data/office-data/src/main/java/com/clip/notice/repository/BannerRepository.java
@@ -2,6 +2,21 @@ package com.clip.notice.repository;
 
 import com.clip.notice.entity.Banner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 public interface BannerRepository extends JpaRepository<Banner, Long> {
+
+    @Query("select b from Banner b where b.id = :bannerId")
+    Optional<Banner> findBanner(@Param("bannerId") Long bannerId);
+
+
+    @Transactional
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("delete from Banner b where b.id = :bannerId")
+    void deleteBanner(@Param("bannerId")Long bannerId);
 }
