4f5219225804c45e9fb4fe60a8bb5309ad44c8fc
W.llama <tmdqja96@gmail.com>
2025-04-11 20:59:01 +0900

    updateBannerDto 작성

---
+++ boot/office/src/main/java/com/clip/office/notice/controller/dto/UpdateBannerDto.java
@@ -0,0 +1,32 @@
+package com.clip.office.notice.controller.dto;
+
+import com.clip.notice.entity.BannerType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+public class UpdateBannerDto {
+
+    private Long id;
+    private BannerType bannerType;
+    private String head;
+    private String sub;
+    private String imageUrl;
+    private LocalDate exposureDate;
+    private boolean isExposure;
+
+    @Builder
+    public UpdateBannerDto(Long id,BannerType bannerType, String head, String sub,String imageUrl, LocalDate exposureDate, boolean isExposure) {
+        this.id = id;
+        this.bannerType = bannerType;
+        this.head = head;
+        this.sub = sub;
+        this.imageUrl = imageUrl;
+        this.exposureDate = exposureDate;
+        this.isExposure = isExposure;
+    }
+}
