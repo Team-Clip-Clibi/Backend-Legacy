fe861cd830567af336492a38b07f499387477fcb
W.llama <tmdqja96@gmail.com>
2025-04-17 02:32:55 +0900

    feat: OneThingMatching repository

--- data/core-data/src/main/java/com/clip/matching/repository/OneThingMatchingRepository.java
+++ data/core-data/src/main/java/com/clip/matching/repository/OneThingMatchingRepository.java
@@ -2,6 +2,14 @@ package com.clip.matching.repository;
 
 import com.clip.matching.entity.OneThingMatching;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface OneThingMatchingRepository extends JpaRepository<OneThingMatching, Long> {
+    @Transactional
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("DELETE FROM OneThingMatching rm WHERE rm.id = :oneThingMatchingId")
+    void deleteOneThingMatching(@Param("oneThingMatchingId")Long oneThingMatchingId);
 }
