4e16ad5716336a828f568bfc6f20b8b0b8918369
jang-199 <jw1010110@gmail.com>
2025-04-17 19:35:26 +0900

    Repository

--- data/core-data/src/main/java/com/clip/matching/repository/UserRandomMatchingRepository.java
+++ data/core-data/src/main/java/com/clip/matching/repository/UserRandomMatchingRepository.java
@@ -2,10 +2,14 @@ package com.clip.matching.repository;
 
 import com.clip.matching.entity.UserRandomMatching;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRandomMatchingRepository extends JpaRepository<UserRandomMatching, Long> {
 
@@ -18,4 +22,28 @@ public interface UserRandomMatchingRepository extends JpaRepository<UserRandomMa
             order by u.randomMatching.meetingTime asc
             """)
     List<UserRandomMatching> findUserRandomMatching(Long userId, LocalDateTime date);
+
+    @Query("""
+            select u
+            from UserRandomMatching u
+            join fetch u.randomMatching
+            where u.user.id = :userId
+            and u.randomMatching.meetingTime >= :dateTime
+            order by u.randomMatching.meetingTime desc
+            limit 1
+            """)
+    Optional<UserRandomMatching> findLatestUserRandomMatching(@Param("userId") long userId, @Param("dateTime") LocalDateTime dateTime);
+
+    @Query("""
+            select u
+            from UserRandomMatching u
+            join fetch u.user
+            where u.randomMatching.id = :randomMatchingId
+            """)
+    List<UserRandomMatching> findUserRandomMatching(@Param("randomMatchingId") long randomMatchingId);
+
+    @Transactional
+    @Modifying
+    @Query("update UserRandomMatching u set u.isCheckedMatchingStart = true where u.user.id = :userId and u.id = :id")
+    void updateStatusChecked(@Param("userId") long userId, @Param("id") long userRandomMatchingId);
 }
