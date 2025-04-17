7640ef07b5806d9045dd97b771b6e111c6e3e51b
jang-199 <jw1010110@gmail.com>
2025-04-17 19:29:40 +0900

    API Docs

--- boot/api/src/main/java/com/clip/api/docs/matching/UserMatchingDocs.java
+++ boot/api/src/main/java/com/clip/api/docs/matching/UserMatchingDocs.java
@@ -1,16 +1,19 @@
 package com.clip.api.docs.matching;
 
+import com.clip.api.matching.controller.dto.MatchingProgressStatusDto;
 import com.clip.api.matching.controller.dto.MatchingSummaryDto;
+import com.clip.api.matching.controller.dto.MatchingType;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "매칭 관리", description = "매칭 신청, 조회 etc ")
 @RequestMapping("/matchings")
@@ -35,4 +38,41 @@ public interface UserMatchingDocs {
     @GetMapping("/summaries")
     MatchingSummaryDto getMatchingSummaryInfo(@AuthenticationPrincipal UserDetails userDetails);
 
+    @Operation(
+            summary = "진행중인 모임 정보 조회",
+            description = """
+                    - 홈 화면에서 사용되는 API입니다.
+                    - 진행중인 모임이 있는 경우 홈화면 하단에 노출되는 정보를 조회하는 API입니다.
+                   \s""",
+            security = @SecurityRequirement(name = "Bearer Token")
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "조회 성공",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = MatchingProgressStatusDto.class)
+            )
+    )
+    @GetMapping("/progress-status")
+    MatchingProgressStatusDto getMatchingStatus(@AuthenticationPrincipal UserDetails userDetails);
+
+    @Operation(
+            summary = "진행중인 모임 정보 조회 읽음 상태로 변경",
+            description = """
+                     - 홈 화면에서 사용되는 API입니다.
+                     - 진행중인 모임이 있는 경우 홈화면 하단에 노출되는 정보를 조회한 상태로 업데이트하는 API입니다.
+                     - URL 경로의 matchingType은 ONE_THING, RANDOM 중 하나입니다.
+                    \s""",
+            security = @SecurityRequirement(name = "Bearer Token")
+    )
+    @ApiResponse(
+            responseCode = "204",
+            description = "업데이트 성공"
+    )
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/{matchingType}/{matchingId}/progress-status/check")
+    void updateMatchingStatusChecked(@PathVariable MatchingType matchingType,
+                                     @PathVariable long matchingId,
+                                     @AuthenticationPrincipal UserDetails userDetails);
 }
