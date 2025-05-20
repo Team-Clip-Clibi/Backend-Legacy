1f0290e1575e8fa6d6348f5665c3d0c9b634950c
isprogrammingfun <wisjang1@naver.com>
2025-05-20 22:27:33 +0900

    refactor : Modifying 추가

--- boot/api/src/main/java/com/clip/batch/matching/repository/RandomMatchingOrderBatchRepository.java
+++ boot/api/src/main/java/com/clip/batch/matching/repository/RandomMatchingOrderBatchRepository.java
@@ -1,6 +1,7 @@
 package com.clip.batch.matching.repository;
 
 import com.clip.order.repository.RandomOrderRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 public interface RandomMatchingOrderBatchRepository extends RandomOrderRepository {
 
     @Transactional
+    @Modifying
     @Query("delete from RandomOrder r where r.expiredAt < :now")
     void deleteExpiredOrder(@Param("now") LocalDateTime now);
 }
