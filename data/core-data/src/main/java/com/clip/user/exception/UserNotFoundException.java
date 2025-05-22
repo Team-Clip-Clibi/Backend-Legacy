4e0c06cb7e7413220f7f0bc530db0d8acc9cfc3c
jang-199 <jw1010110@gmail.com>
2025-05-22 16:52:59 +0900

    NotFound, AlreadyExist, NoContent 계열 예외 통일을 위해 삭제

--- data/core-data/src/main/java/com/clip/user/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.clip.user.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException() {
-        super("유저 정보를 찾을 수 없습니다.");
-    }
-}
