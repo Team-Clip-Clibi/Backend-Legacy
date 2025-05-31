a946df2bf451bee887f9896c0e8470bd49349c89
jang-199 <jw1010110@gmail.com>
2025-05-31 18:02:13 +0900

    Style: @Slf4j 삭제

--- boot/api/src/main/java/com/clip/api/matching/service/OneThingMatchingOrderService.java
+++ boot/api/src/main/java/com/clip/api/matching/service/OneThingMatchingOrderService.java
@@ -14,14 +14,11 @@ import com.clip.user.entity.User;
 import com.clip.user.service.UserService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.temporal.TemporalAdjusters;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class OneThingMatchingOrderService {
