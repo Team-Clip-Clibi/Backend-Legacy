d9ac95d560093916489dc87ad1bc56950caac109
isprogrammingfun <wisjang1@naver.com>
2025-06-01 15:53:04 +0900

    refactor : 미사용 푸시 알림 타입 제거

--- infra/src/main/java/com/clip/infra/fcm/event/FcmNotificationEventListener.java
+++ infra/src/main/java/com/clip/infra/fcm/event/FcmNotificationEventListener.java
@@ -18,12 +18,10 @@ public class FcmNotificationEventListener {
     public void sendGeneralFCM(FcmNotificationEvent.GeneralFcmBatchSendEvent fcmEvent) {
         switch (fcmEvent.getMessageTemplateType()) {
             case MATCHING_COMPLETED -> sendFCMService.sendMatchingCompletedMsg(fcmEvent);
-            case MATCHING_INFO_OPENED -> sendFCMService.sendMatchingInfoOpenedMsg(fcmEvent);
             case MATCHING_TOMORROW -> sendFCMService.sendMatchingTomorrowMsg(fcmEvent);
             case MATCHING_TODAY -> sendFCMService.sendMatchingTodayMsg(fcmEvent);
             case MATCHING_ENDED -> sendFCMService.sendMatchingFinishedMsg(fcmEvent);
             case MATCHING_STARTED -> sendFCMService.sendMatchingStartedMsg(fcmEvent);
-            case MATCHING_MEETUP -> sendFCMService.sendMatchingMeetupMsg(fcmEvent);
         }
     }
 }
