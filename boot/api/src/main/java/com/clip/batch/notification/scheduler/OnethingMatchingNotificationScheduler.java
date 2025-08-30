a0e1fa5d4af93620951bc1574f7c712d114539ce
isprogrammingfun <wisjang1@naver.com>
2025-08-30 09:33:13 +0900

    스케줄러 시간 설정 복구

--- boot/api/src/main/java/com/clip/batch/notification/scheduler/OnethingMatchingNotificationScheduler.java
+++ boot/api/src/main/java/com/clip/batch/notification/scheduler/OnethingMatchingNotificationScheduler.java
@@ -22,8 +22,7 @@ public class OnethingMatchingNotificationScheduler {
     private final Job sendOneThingMatchingInfoFcmJob;
     private final JobExplorer jobExplorer;
 
-    @Scheduled(cron = "*/10 * * * * *") // 1일 전 알림 스케줄러 (금/토)
-//    @Scheduled(cron = "0 0 19 * * FRI,SAT") // 1일 전 알림 스케줄러 (금/토)
+    @Scheduled(cron = "0 0 19 * * FRI,SAT") // 1일 전 알림 스케줄러 (금/토)
     @SchedulerLock(name = "oneThingMatchingInfo_oneDayPrior", lockAtMostFor = "5m", lockAtLeastFor = "1m")
     public void runOneDayPriorJob() throws Exception {
         JobParameters params = new JobParametersBuilder(jobExplorer)
@@ -35,8 +34,7 @@ public class OnethingMatchingNotificationScheduler {
         jobLauncher.run(sendOneThingMatchingInfoFcmJob, params);
     }
 
-    @Scheduled(cron = "*/10 * * * * *") // 당일 알림 스케줄러 (토/일)
-//    @Scheduled(cron = "0 0 9 * * SAT,SUN") // 당일 알림 스케줄러 (토/일)
+    @Scheduled(cron = "0 0 9 * * SAT,SUN") // 당일 알림 스케줄러 (토/일)
     @SchedulerLock(name = "oneThingMatchingInfo_today", lockAtMostFor = "5m", lockAtLeastFor = "1m")
     public void runTodayJob() throws Exception {
         JobParameters params = new JobParametersBuilder(jobExplorer)
